3f4a456f2cfbb4e71164f0eb0cc5c6993eaec48e
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-07-10 20:36:59 -0300

    feat: remove layouts, add defaults

---
+++ src/app/admin/dashboard/accounts/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>Specific Account!</h1>
+  )
+}
---
+++ src/app/admin/dashboard/accounts/page.tsx
@@ -0,0 +1,5 @@
+export default function Accounts() {
+  return (
+    <h1>Accounts!</h1>
+  )
+}
